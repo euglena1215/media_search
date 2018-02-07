9f9b1166ff9451a943662dd302b63150d649eb76
Shintani Teppei <teppest1215@gmail.com>
2018-02-08 00:46:29 +0900

    ドキュメントを検索できるようにした

--- app.rb
+++ app.rb
@@ -8,53 +8,20 @@ require "sinatra/reloader"
 set :database, adapter: "sqlite3", database: "media_search.sqlite3"
 
 # modelの読み込み
-require "./models/image.rb"
-require "./services/label.rb"
+require "./models/document.rb"
 
 module MediaSearch
   class App < Sinatra::Base
-    register Sinatra::FormKeeper
-
     get "/" do
-      images = Image.all
-      erb :index, layout: :layout, locals: { images: images }
-    end
-
-    get "/new" do
-      erb :new
+      documents = Document.all
+      erb :index, layout: :layout, locals: { documents: documents }
     end
 
     get "/search" do
-      id = params[:image].to_i
-      results = Image.sort_by_similar_rgb(id)
-      images = Image.all
+      results = Document.where('title like ? OR content like ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%").order(page_rank: :desc)
+      documents = Document.all
 
-      erb :index, layout: :layout, locals: { images: images, results: results }
-    end
-
-    get "/search_keyword" do
-      results = Image.sort_by_keyword(params[:keyword])
-
-      erb :index, layout: :layout, locals: { images: Image.all, results: results }
-    end
-
-    post "/create" do
-      form do
-        filters :strip
-        field :title,   :present => true
-        field :author, :present => true
-        field :url, :present => true
-      end
-      if form.failed?
-        output = erb :new
-        fill_in_form(output)
-      else
-        labels, scores = Label.detect_labels(form[:url])
-
-        Image.create(title: form[:title], author: form[:author], url: form[:url], labels: labels.join("/"), scores: scores.join("/"))
-
-        redirect '/'
-      end
+      erb :index, layout: :layout, locals: { documents: documents, results: results }
     end
   end
 end
